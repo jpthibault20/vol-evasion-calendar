82ecc337df7cf8801257a4912a87ed19e2f1fe96
thibault <tjeanpierre757@gmail.com>
2024-05-08 20:20:41 +0200

    daily commit

--- components/addStudentToAppointment.tsx
+++ components/addStudentToAppointment.tsx
@@ -5,6 +5,8 @@ import { CardWrapper } from "./CardWrapper";
 import { Appointment } from "@prisma/client";
 import { Spinner } from "./ui/spinner";
 import { getAppointment } from "@/actions/appointment";
+import { Button } from "./ui/button";
+import { FormError } from "./form-error";
 
 interface AddStudentProps {
     view: boolean;
@@ -15,6 +17,7 @@ interface AddStudentProps {
 
 export const AddStudent = ({ view, setView, appointmentID }: AddStudentProps) => {
     const [appointment, setAppointment] = useState<Appointment | null>(null);
+    const [error, setError] = useState<string | undefined>();
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
@@ -30,6 +33,10 @@ export const AddStudent = ({ view, setView, appointmentID }: AddStudentProps) =>
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [view]);
 
+    const handleSubmit = () => {
+
+    }
+
 
     if (!view) {
         return null;
@@ -41,9 +48,24 @@ export const AddStudent = ({ view, setView, appointmentID }: AddStudentProps) =>
                 {isLoading ? (
                     <Spinner>Chargement ...</Spinner>
                 ) : (
-                    <div>
+                    <div className="space-y-6">
+                        <p className="text-gray-500 text-center mb-5">Ajouter un Elève à ce vol ?</p>
 
-                        <p>{appointment?.startDate?.toLocaleDateString("fr-FR")} {appointment?.startDate?.toLocaleTimeString()}</p>
+                        <form className="space-y-6">
+                            <label>Liste des élève</label>
+                            <select
+                                name="Role"
+                                className="block w-full pl-3 pr-10 py-2 text-base border border-gray-300 rounded-md bg-white text-gray-900 disabled:bg-gray-100 disabled:text-gray-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+                            >
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="C">C</option>
+                            </select>
+
+                            <FormError message={error} />
+                            <Button>Enregistrer</Button>
+                        </form>
+
                     </div>
                 )}
             </CardWrapper>
