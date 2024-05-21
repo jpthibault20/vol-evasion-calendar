b966ef0b956092f53c5f977fd83c8b5705800a1c
jpthibault20 <tjeanpierre757@gmail.com>
2024-05-21 14:55:00 +0200

    "Update newAppointment action: modify error messages, add recurrence validation, and adjust date calculations"

--- actions/new-appointment.ts
+++ actions/new-appointment.ts
@@ -39,7 +39,7 @@ export const newAppointment = async (values: z.infer<typeof NewAppointment>) => 
         // console.log("date : ", new Date())
         // console.log("timeStart : ", timeStart)
         if (new Date().getHours() >= timeStart.getHours()) {
-            return { error: "Horaire début erroné (min + 1h)" }
+            return { error: "Horaire début erroné (min + 1h) ou heure passée" }
         }
     }
 
@@ -64,9 +64,15 @@ export const newAppointment = async (values: z.infer<typeof NewAppointment>) => 
     dateEndReccurence?.setHours(25);
     dateEndReccurence?.setMinutes(59);
 
-if (!dateEndReccurence) {
-    return {error:"Date de fin invalide"}
-}
+    console.log(timeEnd.getTime() - timeStart.getTime())
+
+    if (recurrence && timeEnd.getTime() - timeStart.getTime() > 3600000) {
+        return {error:"1h max pour une recurence"}
+    }
+
+    if (recurrence && !dateEndReccurence) {
+        return { error: "Date de fin invalide" }
+    }
 
     if (recurrence) {
         if (dateEndReccurence!.getTime() < date.getTime() + (1209600000 - 86400000)) {
@@ -99,7 +105,7 @@ if (!dateEndReccurence) {
         };
 
         const useDateStart = date;
-        const useDateEnd = new Date(useDateStart.getTime() + 3600000 );
+        const useDateEnd = new Date(useDateStart.getTime() + 3600000);
 
         while (useDateStart < timeEnd) {
             await db.appointment.create({
@@ -147,7 +153,7 @@ if (!dateEndReccurence) {
                 if (appointmentAlreadyExiste.length !== 0) {
                     return { error: `Indisponibilité déja présente avec c'est dates : ${useStartDate.toDateString()} - ${useEndDate.toDateString()}` }
                 };
-                
+
 
                 await db.appointment.create({
                     data: {
@@ -165,7 +171,7 @@ if (!dateEndReccurence) {
 
 
 
-                
+
                 useStartDate.setTime(useStartDate.getTime() + oneHourMs);
                 useEndDate.setTime(useStartDate.getTime() + oneHourMs);
             }
