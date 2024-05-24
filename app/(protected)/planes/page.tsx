1cd9e2e46479a6e4f5a83638ef6f80793da54602
jpthibault20 <tjeanpierre757@gmail.com>
2024-05-24 19:13:16 +0200

    "Updated sidebar link text, added image to planes page, and modified role gate component to display unauthorized access image"

--- app/(protected)/planes/page.tsx
+++ app/(protected)/planes/page.tsx
@@ -1,5 +1,7 @@
 import { RoleGate } from '@/components/auth/role-gate'
+import Image from 'next/image'
 import React from 'react'
+import pictureBuilding from "@/public/Under construction.gif"
 
 const planes = () => {
   return (
@@ -7,7 +9,11 @@ const planes = () => {
       <RoleGate allowedRole={'PILOTE'} noAccesPage={true}>
         <div className="flex justify-center items-center h-screen">
           <p className="text-center">
-            Page indisponible pour le moment
+            <p>Cette page est en construction</p>
+            <Image 
+            src={pictureBuilding}
+            alt='picture website in building'
+            width={500}/>
           </p>
         </div>
       </RoleGate>
